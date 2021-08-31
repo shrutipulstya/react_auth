47c3dd8f60f754841050342e958e3c7a32fc48b6
Shruti Pulstya <shrutipulstya9@gmail.com>
2021-08-31 17:59:26 +0530

    added a signUp form

--- src/components/signUp.component.js
+++ src/components/signUp.component.js
@@ -1,9 +1,34 @@
+
 import React, {Component} from 'react';
 
 export default class SignUp extends Component {
     render() {
         return(
-            <h2>This is the SignUp page</h2>
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>Full Name</label>
+                    <input type="text" className="form-control" placeholder = "Full Name"/>
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder = "Email"/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder = "Password"/>
+                </div>
+
+                <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" className="form-control" placeholder = "Confirm Password"/>
+                </div>
+
+                <button className="btn btn-outline-primary mt-4">Sign Up</button>
+            </form>
         )
     };
 }
